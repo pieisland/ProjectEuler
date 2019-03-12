af5e374e1b818d907a56bbb0c651553854f8738f
DESKTOP-35R8C3Q\pieis <qorwn79@gmail.com>
2019-03-12 22:22:08 +0900

    1001번째 소수. 시간 너무 오래 걸림

---
+++ pro7.py
@@ -0,0 +1,34 @@
+#coding:utf-8
+
+"""
+2019.03.12.Tue.
+<1001번쨰 소수>
+딱 보니까 에라토스테네스의 체
+인 것 같기는 한데 몇 번쨰의 소수를 구하라고 했기 때문에..
+조금 애매한데..??
+"""
+def findPrime(n):
+
+    for i in range(2, n):
+        if n%i==0:
+            #print("It is not prime number")
+            return 0
+    
+    #print("It is prime number")
+    return 1
+
+
+def main():
+    a=[]
+
+    cnt=1
+    num=2
+    while(cnt!=10001):
+        num+=1
+        n=findPrime(num)
+        if n==1: cnt+=1
+        
+    print(num)
+    
+if __name__=="__main__":
+    main()
